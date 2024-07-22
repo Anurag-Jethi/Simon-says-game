d2833c2d63eedca4251efcc04655973011ace8cb
Anurag-Jethi <anuragjethi003@gmail.com>
2024-07-23 00:12:39 +0530

    added level 1 and flash function

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ let userSeq = [];
 
 let started = false;
 let level = 0;
+let btns = ["red", "yellow", "purple", "green"];
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function(){
     if(started == false){
         started = true;
+        levelUp();
     }
 })
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 350);
+}
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    btnFlash(randBtn);
+}
